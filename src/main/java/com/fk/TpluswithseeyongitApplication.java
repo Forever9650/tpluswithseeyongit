73b45ba64de628f1e9a7d884be9f52ca7b3d8d8f
kaikai <252129650@qq.com>
2023-04-20 21:56:00 +0800

    Initial commit

---
+++ src/main/java/com/fk/TpluswithseeyongitApplication.java
@@ -0,0 +1,13 @@
+package com.fk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpluswithseeyongitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpluswithseeyongitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fk/TpluswithseeyongitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpluswithseeyongitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
